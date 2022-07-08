9951c2bbb2ae3b211694755de5adc1791ba41a93
Itz Pongz <rapeeban42@gmail.com>
2022-07-08 18:19:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+(function(){
+    document.documentElement.style.transitionDuration="360s";
+    document.documentElement.style.transitionTimingFunction="ease-in";
+    document.documentElement.style.transform="rotate(360deg)";
+}());
